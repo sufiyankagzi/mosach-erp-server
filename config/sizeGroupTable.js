82a0ea87d213ba55b27e3554953c41fba76ccf97
sufiyankagzi <49756873+sufiyankagzi@users.noreply.github.com>
2026-08-20 20:36:50 +0530

    Size Group Table

---
+++ config/sizeGroupTable.js
@@ -0,0 +1,20 @@
+const db = require("../config/connectdb")
+
+const createSizeGroupTable = () => {
+    const sql = `
+        CREATE TABLE IF NOT EXISTS color (
+    sizegorupid INT AUTO_INCREMENT PRIMARY KEY,
+    sizegroup VARCHAR(100) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+) `;
+    db.query(sql, (err) => {
+        if (err) {
+            console.log("Size Group Table Error:", err);
+        } else {
+            console.log("Size group table ready.");
+        }
+
+    })
+}
+
+module.exports = createSizeGroupTable;
